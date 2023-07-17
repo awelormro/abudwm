a27e59d68ff89c00a427d0ee5c1225a2355f449e
awelormro <ssuuppeerr@live.com.mx>
2023-07-17 13:34:41 -0600

    update keybinds

--- config.def.h
+++ config.def.h
@@ -84,14 +84,17 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 /* static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL }; */
 static const char *dmenucmd[] = { "bash", "/home/abu/.config/polybar/grayblocks/scripts/launcher.sh"};
 
-static const char *srcshotcmd[] = { "bash", "/home/abu/.config/rofi/applets/bin/screenshot.sh"};
+static const char *srcshotcmd[] = { "bash", "/home/abu/.config/rofi/applets/bin/screenshot.sh", NULL};
 static const char *termcmd[]  = { "st", NULL };
+static const char *lockcmd[] = { "dm-tool", "lock", NULL};
+
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
   { MODKEY,                       XK_u,      spawn,          {.v =srcshotcmd} },
+  { MODKEY|ShiftMask,             XK_x,      spawn,          {.v =lockcmd} },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
