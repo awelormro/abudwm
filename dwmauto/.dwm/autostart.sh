94f4ba742a0203c9005428cddf68d2b77529102e
awelormro <ssuuppeerr@live.com.mx>
2023-11-27 14:54:19 -0600

    update start of screen

--- dwmauto/.dwm/autostart.sh
+++ dwmauto/.dwm/autostart.sh
@@ -1,4 +1,5 @@
 sleep 5 && exec ~/.screenlayout/configuses.sh &
+xrandr --output HDMI-A-0 --mode 1920x1080 --output eDP --mode 1366x768 --rate 60.00 --right-of HDMI-A-0 &
 nm-applet &
 # exec  /home/abu/.config/polybar/launch.sh --grayblocks &
 # exec  /home/abu/.config/polybar/grayblocks/scripts/pywal.sh /home/abu/wallpapers &
